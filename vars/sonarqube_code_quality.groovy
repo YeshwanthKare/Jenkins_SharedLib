3450a211db2e0fcbb43bffeb4ef8c64b8bc23878
Jaya Bhavan Yeswant Nadh Kare <64756016+YeshwanthKare@users.noreply.github.com>
2024-09-25 17:15:07 +0200

    Update sonarqube_code_quality.groovy

--- vars/sonarqube_code_quality.groovy
+++ vars/sonarqube_code_quality.groovy
@@ -1,5 +1,15 @@
-def call(){
-  timeout(time: 20, unit: "MINUTES"){
-      waitForQualityGate abortPipeline: false
+def call() {
+  timeout(time: 20, unit: "MINUTES") {
+      def result = waitForQualityGate abortPipeline: false
+      echo "SonarQube Quality Gate result: ${result}"
+
+      if (result.status == 'ERROR') {
+          error("Quality gate check failed. Please fix issues.")
+      } else if (result.status == 'WARN') {
+          echo "Quality gate check passed with warnings."
+      } else {
+          echo "Quality gate check passed successfully."
+      }
   }
 }
+
